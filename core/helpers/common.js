f9f3b5f6e7a20e2fa709452c565b791767636e01
Prem <joshiprem978@gmail.com>
2024-01-14 13:45:03 +0530

    added view function defination

--- core/helpers/common.js
+++ core/helpers/common.js
@@ -1,5 +1,10 @@
 import View from "../libraries/View.js";
 
+/**
+ * view - load and display view file.
+ * @param {string} path Path of your view file in views foolder.
+ * @param {object} data Pass data to your view file.
+ */
 export function view(path, data){
     View.load(path, data);
 }
